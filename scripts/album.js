49b0acdd6ef3b42673b10a5cbe1a6df2b4897a89
kba239 <adams.kate.b@gmail.com>
2016-10-02 16:06:27 -0400

    Assignment 20 Buzz Library: completed

--- scripts/album.js
+++ scripts/album.js
@@ -185,7 +185,22 @@ var updatePlayerBarSong = function() {
     $(".currently-playing .artist-name").text(currentAlbum.artist);
     $(".currently-playing .artist-song-mobile").text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
     $('.main-controls .play-pause').html(playerBarPauseButton);
+};
+
+var togglePlayFromPlayerBar = function() {
+    if (currentSoundFile.isPaused() && $playPause.click()) {
+    $(this).html(pauseButtonTemplate); 
+    $('.main-controls .play-pause').html(playerBarPauseButton);
+    setSong(songNumber);
+    currentSoundFile.play());
+    } else {
+    $(this).html(playButtonTemplate); 
+    $('.main-controls .play-pause').html(playerBarPlayButton);
+    currentSoundFile.pause();
+    }  
 }
+
+
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
@@ -200,9 +215,11 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPause = $('.main-controls .play-pause');
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
+    $playPause.click(.togglePlayFromPlayerBar());
 });
